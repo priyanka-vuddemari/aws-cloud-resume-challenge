20252a1db1aaed5dd17268b420f650f9c6814abc
Priyanka Vuddemari <58094868+priyanka-vuddemari@users.noreply.github.com>
2026-09-24 19:49:16 +0000

    Add remote Terraform backend using S3

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,8 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    
   }
   required_version = ">= 1.0"
+  backend "s3" {
+    bucket = "priyanka-terraform-state-bk"
+    key    = "cloud-resume/terraform.tfstate"
+    region = "ap-south-2"
+}
 }
 
 provider "aws" {
